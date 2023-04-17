49c25c96ff1e0369b1b9ac9d9606fc9e6d81fd2f
hien.nguyen <mynameishieenf@gmail.com.com>
2023-04-17 23:02:32 +0700

    change files

---
+++ messages/src/messages/messages.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  constructor(public messagesService: MessagesService) {
+    this.messagesService = messagesService;
+  }
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/:id')
+  async getMessages(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    console.log(message);
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
+  }
+}
